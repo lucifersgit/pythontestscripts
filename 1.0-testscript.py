8a4f45b10d25cfedc7ed054b63b2509c77e908bd
Lucifer <louie.francis@hotmail.com>
2018-08-28 21:29:07 +0100

    updated test

---
+++ 1.0-testscript.py
@@ -0,0 +1,14 @@
+### This is a test script to read a file, and print out those whose age is over a stated value
+
+def personal_details():
+    name = "Simon"
+    age =  int(19)
+    print("Name: {}\nAge: {}".format(name, age))
+
+def personal_details2():
+    name = "Stuart"
+    age = int(13)
+    print("Name: {}\nAge: {}".format(name, age))
+
+personal_details()
+personal_details2()
