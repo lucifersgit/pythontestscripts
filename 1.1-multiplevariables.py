d3a9199eabffc6a518cd8ecf8f03fbdbb112eeb9
Lucifer <louie.francis@hotmail.com>
2018-08-28 21:46:39 +0100

    1 more

---
+++ 1.1-multiplevariables.py
@@ -0,0 +1,5 @@
+### This is a test script to read a file, and print out those whose age is over a stated value
+
+nameandage = {'John': 42, 'Tom': 12, 'David': 25}
+
+print (nameandage) # print the values of nameandage.
